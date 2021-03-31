316f0d23c18711eab6afcaf4a439aae4935a92f1
TwiDev <twidev5@gmail.com>
2021-03-31 15:11:20 +0200

    add gradle project and libs

---
+++ src/main/java/net/twidev/itemsCreator/Creator.java
@@ -0,0 +1,16 @@
+package net.twidev.itemsCreator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Creator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
